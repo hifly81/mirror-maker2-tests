c45e7a73128df62bb42f44dd6bc4d745bf74d798
hifly81 <gio.marigi@gmail.com>
2020-09-11 14:35:33 +0200

    additional conf

--- kafka-cluster-2/include.sh
+++ kafka-cluster-2/include.sh
@@ -1,4 +1,4 @@
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-KAFKA_DIR=
+KAFKA_DIR=/home/hifly/Software/kafka/kafka_2.13-2.4.0-bis
 BROKER_CONFIG_DIR="${MY_DIR}/config"
 ZOO_CONFIG_DIR="${MY_DIR}/config"
